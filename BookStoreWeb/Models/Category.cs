1fa05b0b1465a6ed787eb6356c12331a939f207e
Garden <66628342+ljw9611@users.noreply.github.com>
2022-08-20 02:49:36 +0900

    초기 커밋.

---
+++ BookStoreWeb/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace BookStoreWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder{ get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
