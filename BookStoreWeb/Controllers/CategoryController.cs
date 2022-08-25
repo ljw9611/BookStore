cedd8f56f2c6ca985af6be6c261f8d8e89ce1b06
Garden <66628342+ljw9611@users.noreply.github.com>
2022-08-25 22:52:23 +0900

    DB 연결

---
+++ BookStoreWeb/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BookStoreWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var objCategoryList = _db.Categories.ToList();
+            return View();
+        }
+    }
+}
