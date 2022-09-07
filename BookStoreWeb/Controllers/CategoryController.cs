219f35c26cfab56fa2f2ebb4f925209e798bd729
Garden <66628342+ljw9611@users.noreply.github.com>
2022-09-07 23:56:51 +0900

    CSS Update

--- BookStoreWeb/Controllers/CategoryController.cs
+++ BookStoreWeb/Controllers/CategoryController.cs
@@ -18,5 +18,21 @@ namespace BookStoreWeb.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return View();
+        }
     }
 }
